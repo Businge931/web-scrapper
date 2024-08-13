c2d13e4ccf90691c31c26423b302235b51b353af
Businge931 <jimmybusinge9@gmail.com>
2024-08-13 10:36:16 +0300

    feat: added search company name func by name on Google

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
-	
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func readCompanyNames(filename string) ([]string, error) {
@@ -22,7 +26,46 @@ func readCompanyNames(filename string) ([]string, error) {
 	return companies, scanner.Err()
 }
 
+func searchGoogle(companyName string) ([]string, error) {
+	baseURL := "https://www.google.com/search"
+	query := url.Values{}
+	query.Add("q", companyName)
+	query.Add("num", "20") // Number of results to return (max 10)
 
+	searchUrl := fmt.Sprintf("%s?%s", baseURL, query.Encode())
+
+	// Make the HTTP request
+	resp, err := http.Get(searchUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to search Google: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	// Check for non-200 status codes
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("non-200 status code: %d", resp.StatusCode)
+	}
+
+	// Parse the HTML response
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse Google search results: %w", err)
+	}
+
+	// Extract URLs from the search results
+	var urls []string
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		link, exists := s.Attr("href")
+		if exists && strings.HasPrefix(link, "url?q=") {
+			// Trim the "/url?q=" prefix and everything after the first "&"
+			url:=strings.Split(link[7:],"&")[0]
+			urls=append(urls, url)
+		}
+	})
+
+	return urls, nil
+}
 
 func main() {
 	companyNames, err := readCompanyNames("companies.txt")
