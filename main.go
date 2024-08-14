aff8fca2455f3751a00409108b63a01a28d9bd39
Businge931 <jimmybusinge9@gmail.com>
2024-08-14 15:38:15 +0300

    feat: looped over company names and returned site URL

--- main.go
+++ main.go
@@ -12,5 +12,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading input file: %v", err)
 	}
-	fmt.Println("compnay names",companyNames)
+	output := make(map[string]string)
+
+	for _, companyName := range companyNames {
+		companyURL, err := scraper.GetSearchResults(companyName)
+		if err != nil {
+			log.Printf("Error getting search results for %s: %v", companyName, err)
+			output[companyName] = ""
+			continue
+		}
+		fmt.Println("company url", companyURL)
+		output[companyName] = companyURL
+
+	}
+
 }
